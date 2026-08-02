afe0fc0ee23dc24176b7d08726391f1ed1605883
apagyizsolt6-ship-it <apagyizsolt6@gmail.com>
2026-08-02 19:48:02 +0200

    Fix import path for ai_simulation_service.dart

--- lib/screens/ai/ai_analysis_screen.dart
+++ lib/screens/ai/ai_analysis_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../services/ai_simulation_service.dart';
+import '../../services/ai_simulation_service.dart';
 
 class AiAnalysisScreen extends StatefulWidget {
   final String homeTeam;
